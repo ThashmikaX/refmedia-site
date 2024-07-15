a5a0e6016ed511e0a87d13e81f6b7e6434665ec2
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-07-16 00:38:38 +0530

    Nav bar partially done.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,180 @@
+"use client"
+import React, { useState, useEffect } from "react"
+import clsx from "clsx"
+import Link from "next/link"
+
+const navItems = [
+  { label: "Home", href: "home" },
+  { label: "Gallery", href: "gallery" },
+  { label: "About", href: "about" },
+  { label: "Contact", href: "contact" }
+]
+
+function Navbar() {
+  const scrolltoHash = function (element_id: string) {
+    const element = document.getElementById(element_id)
+
+    if (!element) {
+      globalThis.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+    }
+
+    element?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "nearest"
+    })
+  }
+
+  const elementIsVisibleInViewport = (el: HTMLElement | null, partiallyVisible = true) => {
+    if(!el) return false
+    const { top, left, bottom, right } = el.getBoundingClientRect();
+    const { innerHeight, innerWidth } = window;
+    return partiallyVisible
+      ? ((top > 0 && top < innerHeight) ||
+          (bottom > 0 && bottom < innerHeight)) &&
+          ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+      : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
+  };
+
+    
+  const setActiveLinks = () => {
+
+    if (elementIsVisibleInViewport(document.getElementById("home"))) {
+        setActiveLink("Home")
+    } 
+    if (elementIsVisibleInViewport(document.getElementById("gallery"))) {
+        setActiveLink("Gallery")
+    } 
+    if (elementIsVisibleInViewport(document.getElementById("about"))) {
+        setActiveLink("About")
+    } 
+    if (elementIsVisibleInViewport(document.getElementById("contact"))) {
+        setActiveLink("Contact")
+    } 
+  }
+
+  const [activeLink, setActiveLink] = useState("Home")
+
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false)
+
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen)
+  }
+
+  const [hasScrolled, setHasScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setHasScrolled(true)
+      } else {
+        setHasScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    window.addEventListener("scroll", setActiveLinks)
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  return (
+    <nav
+      className={"sticky top-0 z-50 flex justify-center "}
+    >
+      <div className="absolute w-full backdrop-blur-lg bg-black bg-opacity-50 lg:rounded-lg lg:m-8 lg:w-[95%]">
+        <div className="container mx-auto px-4 relative lg:text-sm">
+          <div className="py-3 md:py-4 flex justify-between items-center">
+            <div className="flex items-center flex-shrink-0">
+              <img
+                className={"mr-2 duration-500 h-12 md:h-16"}
+                src="/assets/Logo.svg"
+                alt="Logo"
+              />
+            </div>
+            <ul className="hidden lg:flex ml-14 space-x-12 ">
+              {navItems.map((item, index) => (
+                <li
+                  key={index}
+                  className={
+                    activeLink == item.label
+                      ? "bg-text-gradient font-bold text-transparent bg-clip-text"
+                      : undefined
+                  }
+                >
+                  <div
+                    className="hover:cursor-pointer"
+                    onClick={() => {
+                      scrolltoHash(item.href)
+                      setActiveLink(item.label)
+                    }}
+                  >
+                    {item.label}
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className="lg:hidden md:flex flex-col justify-end">
+              <HamburgerMenu
+                isOpen={mobileDrawerOpen}
+                toggleMenu={toggleNavbar}
+              />
+            </div>
+          </div>
+          {mobileDrawerOpen && (
+            <div className="fixed right-0 z-20 bg-[#01224D] w-full pt-2 flex flex-col justify-center lg:hidden">
+              <ul>
+                {navItems.map((item, index) => (
+                  <li
+                    key={index}
+                    className="py-3 pl-8 border-b-2 border-primary-background"
+                  >
+                    <div
+                      className="hover:cursor-pointer"
+                      onClick={() => {
+                        toggleNavbar()
+                        scrolltoHash(item.href)
+                      }}
+                    >
+                      {item.label}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
+
+const HamburgerMenu = (props: { toggleMenu: () => void; isOpen: boolean }) => (
+  <button
+    onClick={props.toggleMenu}
+    className="relative w-6 h-4 flex flex-col justify-between items-center group"
+  >
+    <span
+      className={clsx(
+        "block w-full h-0.5 bg-text-gradient transform transition duration-300",
+        { "rotate-45 translate-y-1.5": props.isOpen }
+      )}
+    ></span>
+    <span
+      className={clsx(
+        "block w-full h-0.5 bg-text-gradient transition duration-300",
+        { "opacity-0": props.isOpen }
+      )}
+    ></span>
+    <span
+      className={clsx(
+        "block w-full h-0.5 bg-text-gradient transform transition duration-300",
+        { "-rotate-45 -translate-y-1.5": props.isOpen }
+      )}
+    ></span>
+  </button>
+)
