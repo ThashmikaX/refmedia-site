2844a0d5eaab65dbd8907ea3e37f5d252ec6fbe1
Janith-Chamikara <janithchamikara13@gmail.com>
2024-07-19 13:36:47 +0530

    update(button): updated button styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,17 +1,23 @@
 type ButtonProps = {
   children: string;
   onClick?: () => void;
+  iconClassName?: string | undefined;
   Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 };
 
-export default function Button({ children, onClick, Icon }: ButtonProps) {
+export default function Button({
+  children,
+  onClick,
+  Icon,
+  iconClassName,
+}: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="flex flex-row shadow-buttonShadowDefault items-center bg-button px-[24px] py-[12px] rounded-[4px] gap-[10px] hover:shadow-buttonShadowHoverd focus:shadow-buttonShadowPressed transition-all ease-in duration-300"
+      className="flex text-white flex-row font-[600px] text-[16px] shadow-buttonShadowDefault items-center bg-button px-[24px] py-[12px] rounded-[4px] gap-[10px] hover:shadow-buttonShadowHoverd focus:shadow-buttonShadowPressed transition-shadow ease-out duration-300 focus:ring-4 focus:ring-button/40"
     >
       <p>{children}</p>
-      {Icon && <Icon />}
+      {Icon && <Icon className={iconClassName} />}
     </button>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,9 +21,13 @@ const config: Config = {
         supportingText: "rgba(0, 0, 0, 0.85)",
         border: "rgba(0, 0, 0, 0.1)",
         button: "rgba(98, 27, 218, 1)",
-        buttonShadowDefault:"0px 0px 1px 1px #5F00FF, 0px 4px 20px 0px rgba(95, 0, 255, 0.45), 0px 16px 20px 0px rgba(95, 0, 255, 0.20)",
-        buttonShadowHoverd:"0px 0px 1px 1px #5F00FF, 0px 4px 20px 0px rgba(95, 0, 255, 0.45)",
-        buttonShadowPressed:"0px 48px 1px 1px rgba(0, 0, 0, 0.20) inset"
+      },
+      boxShadow: {
+        buttonShadowDefault:
+          "0px 0px 1px 1px #5F00FF, 0px 4px 20px 0px rgba(95, 0, 255, 0.45), 0px 16px 20px 0px rgba(95, 0, 255, 0.20)",
+        buttonShadowHoverd:
+          "0px 0px 1px 1px #5F00FF, 0px 4px 20px 0px rgba(95, 0, 255, 0.45)",
+        buttonShadowPressed: "0px 48px 1px 1px rgba(0, 0, 0, 0.20) inset",
       },
     },
   },
