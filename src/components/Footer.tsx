f0dcc6c0725240a801ac255c6af090696d85f724
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-07-25 23:10:45 +0530

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,33 +5,36 @@ import SocialLink from "./SocialLink";
 export default function Footer() {
   return (
     <div className="w-full py-8 bg-gray-50 flex-col justify-start items-center gap-16 inline-flex">
-    <div className="self-stretch h-20 flex-col lg:px-[100px] justify-start items-start gap-16 flex">
+      <div className="self-stretch h-20 flex-col lg:px-[100px] justify-start items-start gap-16 flex">
         <div className="self-stretch h-20 flex-col lg:flex-row justify-between items-center gap-8 flex lg:inline-flex">
-            <div className="justify-start items-center gap-6 inline-flex">
-                <img
-                        className={"w-6 h-6 relative"}
-                        src="/assets/Social/x.svg"
-                        alt="Logo"
-                    />
-                    <img
-                        className={"w-6 h-6 relative"}
-                        src="/assets/Social/linkedin.svg"
-                        alt="Logo"
-                    />
-                    <img
-                        className={"w-6 h-6 relative"}
-                        src="/assets/Social/facebook.svg"
-                        alt="Logo"
-                    />
-                    <img
-                        className={"w-6 h-6 relative"}
-                        src="/assets/Social/github.svg"
-                        alt="Logo"
-                    />  
-            </div>
-            <div className="self-stretch items-center lg:flex text-center text-gray-500 text-base font-normal font-['Inter'] leading-normal">© 2077 REF Media. All rights reserved.</div>
-            </div>
-        </div>
-    </div>
+          <div className="justify-start items-center gap-6 inline-flex">
+            <a href={socialLinks[0].to}>
+              <img
+                className={"w-6 h-6 relative"}
+                src="/assets/Social/facebook.svg"
+                alt="Logo"
+              />
+            </a>
+            <a href={socialLinks[1].to}>
+              <img
+                className={"w-6 h-6 relative"}
+                src="/assets/Social/youtube.svg"
+                alt="Logo"
+              />
+            </a>
+            <a href={socialLinks[2].to}>
+              <img
+                className={"w-6 h-6 relative"}
+                src="/assets/Social/linkedin.svg"
+                alt="Logo"
+              />
+            </a>
+          </div>
+          <div className="self-stretch items-center lg:flex text-center text-gray-500 text-base font-normal font-['Inter'] leading-normal">
+            © 2077 REF Media. All rights reserved.
+          </div>
+      </div>
+      </div>
+      </div>
   );
 }
