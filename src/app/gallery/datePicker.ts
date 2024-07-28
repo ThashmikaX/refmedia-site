906d71554b6962df153e73a2b8a5d8d8aef20523
MalithaJeewaka <144432834+MalithaJeewaka@users.noreply.github.com>
2024-07-28 17:08:48 +0530

    gallery p1

---
+++ src/app/gallery/datePicker.ts
@@ -0,0 +1,23 @@
+export const yearsPick = [
+  { year: 2019, active: false },
+  { year: 2020, active: false },
+  { year: 2021, active: false },
+  { year: 2022, active: false },
+  { year: 2023, active: false },
+  { year: 2024, active: true },
+];
+
+export const monthsPick = [
+  { month: "January", active: false },
+  { month: "February", active: false },
+  { month: "March", active: false },
+  { month: "April", active: false },
+  { month: "May", active: false },
+  { month: "June", active: false },
+  { month: "July", active: false },
+  { month: "August", active: false },
+  { month: "September", active: false },
+  { month: "October", active: false },
+  { month: "November", active: false },
+  { month: "December", active: false },
+];
